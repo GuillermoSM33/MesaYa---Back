7e4d09fadcbf3c325ad52c596e667ec0534b21ed
Jonathan Hernandez <jonathanhernandez@Laptop-de-Jonathan.local>
2025-03-21 06:06:02 -0500

    se genera id del usuario al loguearse en el jwt

--- Interfaces/IAuthService.cs
+++ Interfaces/IAuthService.cs
@@ -4,7 +4,7 @@ namespace MesaYa.Interfaces
 {
     public interface IAuthService
     {
-        string GenerateJWTToken(string username, string email, string role);
+        string GenerateJWTToken(string username, string email, string role, int usuarioId); //inclimos el id del usuario para generarlo bien
         Usuario ValidateUser(string username, string password);
     }
 }
